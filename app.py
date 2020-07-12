7b3df7664b855677aea0753004f8f6715674210b
Diego448 <diegosg448@gmail.com>
2020-07-12 17:24:25 -0500

    Adding initial project files

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from data import db
 
 app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/team/listing')
+def team_listing():
+    return render_template('team_listing.html', team=db)
 
 if __name__ == "__main__":
     app.run(port=5000)
---
+++ data.py
@@ -0,0 +1,4 @@
+db = [
+    {'name': 'Damian', 'position': 'developer', 'language': 'C#', 'technologies': 'C#, .NET, Java'},
+    {'name': 'Desmond', 'position': 'developer', 'language': 'Scala', 'technologies': 'Scala, JVM, Java'}
+]
