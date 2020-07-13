20406d8ae830eebf7de6177a75b972fc14bdc4c6
Diego448 <diegosg448@gmail.com>
2020-07-13 18:34:41 -0500

    Testing frontend code added

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 from data import db
+from utils import get_all_messages, get_auth, get_message
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('messages_all'))
 
 @app.route('/team/listing')
 def team_listing():
     return render_template('team_listing.html', team=db)
+
+@app.route('/messages/all')
+def messages_all():
+    auth_data = get_auth(session['username'])
+    if auth_data['status'] == 'authorized':
+        messages = get_all_messages()
+        return render_template('messages.html', messages=messages)
+    elif auth_data['status'] == 'unauthorized':
+        return render_template('error.html')
+    else:
+        return render_template('error.html')
 
 if __name__ == "__main__":
     app.run(port=5000)
---
+++ utils.py
@@ -0,0 +1,17 @@
+import requests
+
+base_url = 'https://localhost:'
+auth_url = base_url + '5001'
+messages_url = base_url + '5002'
+
+def get_all_messages():
+    r = requests.get(messages_url)
+    return r.json
+
+def get_message(id):
+    r = requests.get(messages_url + '/' + id)
+    return r.json
+
+def get_auth(username):
+    r = requests.get(auth_url + '/' + username)
+    return r.json
